63e4e138744baf18a4adf7bc4e5019e29de63225
goatess <107187023+goatess@users.noreply.github.com>
2022-06-28 20:57:58 +0200

    mensaje

--- src/App.java
+++ src/App.java
@@ -14,53 +14,79 @@ public class App {
             player2 = player.addPlayer();
         }
         System.out.println("Players: " + player1 + " and " + player2);
-
-        // Basic Rules
         
-        // Rolling dice MAIN loop 
+        // Initialize game 
+        String actual_player = "";
+        boolean turncheck = false;
+        int turn = 1;
+        boolean isFinished = false;
         int finishLine = 63;
-        int current_player = 1;
-        boolean turn_change = true;
         Dice dice = new Dice();
-        for (int actualPosition = 1; actualPosition != finishLine;){
-            int position1 = 1;
-            int position2 = 1; 
+        int position1 = 1;
+        int position2 = 1;
+        int new_position = 1;
+        
+        // Dice Roll loop and adding
+        for (isFinished = false; isFinished == false ;){
+            
+            // Turn loop 
+            if (turn == 1 && turncheck == true){
+                turn = 2;
+                actual_player = player1;
+                position2 = player.getPosition();
+                player.setPosition(position2);
+                new_position = position1;
+            } else {
+                turn = 1;
+                actual_player = player2;
+                position1 = player.getPosition();
+                player.setPosition(position1);
+                new_position = position2;
+            }  
+
+            // Checking if player wins or moves, and storing positions
             int d1 = dice.diceRoll();
             dice.setDiceValue1(d1);
             int d2 = dice.diceRoll();
             dice.setDiceValue2(d2);
-            int totalRoll = dice.getDiceAdding();
+            int totalRoll = dice.getDiceAdding();            
+            new_position += totalRoll;
+    
+            if (new_position < finishLine){
+                player.setPosition(new_position);
+                System.out.println( actual_player + " (" + d1 + ", " + d2 + ") Se mueve hasta la casilla: " + player.getPosition());
+                turncheck = true;
             
-            // Turn check system
-            if (current_player == 1 && turn_change == true){
-                current_player = 2;
-                position2 = actualPosition += totalRoll;
-                turn_change = false;
-            }else if (current_player == 2 && turn_change == true){
-                current_player = 1;
-                position1 = actualPosition += totalRoll;
-                turn_change = false;
+            }else if (new_position > finishLine) {
+                new_position = finishLine - (new_position - finishLine);
+                player.setPosition(new_position);
+                System.out.println( actual_player + " (" + d1 + ", " + d2 + ") Se mueve hasta la casilla: " + player.getPosition());
+                turncheck = true;
+            
+            }else {
+                System.out.println(actual_player + "(" + d1 + ", " + d2 + ") Casilla 63! " + actual_player + " WINS!");
+                isFinished = true;
             }
-
-            // Game checks if your roll wins or you move and stores position 
-            if (actualPosition < finishLine){
-                System.out.println(player.newName + " (" + d1 + ", " + d2 + ") Se mueve hasta la casilla: " + actualPosition);
-                turn_change = true;
-            }else if (actualPosition > finishLine) {
-                actualPosition = finishLine - (actualPosition - finishLine);
-                System.out.println(player.newName + " (" + d1 + ", " + d2 + ") Se mueve hasta la casilla: " + actualPosition);
-                turn_change = true;
-            }else System.out.println("(" + d1 + ", " + d2 + ") Casilla 63! " + current_player + " WIN!");
-        }   
-    }
+        }         
+    } 
 }
 
 class Player {
     String newName = "";
+    int position = 1;
+    int finishLine = 63;
+    Dice dice = new Dice();
+    int totalRoll = dice.getDiceAdding();
     String addPlayer(){
         Scanner scanner = new Scanner(System.in);
         newName = scanner.nextLine();
         return newName;
+    }
+    public void setPosition(int position){
+        this.position = position;
+    }
+    public int getPosition(){
+        return position;
     }
 }
 
@@ -69,7 +95,7 @@ class Dice {
     int min = 1;
     int d1 = 1;
     int d2 = 1;
-    
+
     int diceRoll(){
         //Generate random int value from 1 to 6 
         int random_int = (int)Math.floor(Math.random()*(max - min + 1) + min);
@@ -86,3 +112,4 @@ class Dice {
         return diceAdding;
     }
 }
+
